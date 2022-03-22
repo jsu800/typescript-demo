029d31aecc40cfc30eb9e0f56bcab02dec330cac
jsu800 <jsu800@gmail.com>
2022-03-21 21:39:35 -0700

    adding type checking examples

--- HelloWorld/HelloWorld.ts
+++ HelloWorld/HelloWorld.ts
@@ -1,2 +1,11 @@
 let message: string = 'Hello World';
 console.log(message);
+
+// 1st Example: misspelling of the property name
+const obj = {width: 10, height: 15};
+const area = obj.width * obj.heightadadfaf;
+
+// 2nd Example: type re-assignment 
+let amount = 10;
+amount = "Joe"
+console.log(typeof amount);
