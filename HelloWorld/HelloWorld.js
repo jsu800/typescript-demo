b416abc58b042e1ae1a24eb9a6c9ec88e58d8dd7
jsu800 <jsu800@gmail.com>
2022-03-22 18:28:30 -0700

    adding typescript-vsc-demo code plus some typescript changes made to HelloWorld

--- HelloWorld/HelloWorld.js
+++ HelloWorld/HelloWorld.js
@@ -1,2 +1,31 @@
-var message = 'Hello World';
-console.log(message);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const fetch = require("node-fetch");
+var url = 'https://jsonplaceholder.typicode.com/users';
+function getPersons(path) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const persons = yield getData(path);
+        if (persons) {
+            return persons;
+        }
+        return null;
+    });
+}
+function getData(path) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch(path);
+        return yield response.json();
+    });
+}
+getPersons(url).then((persons) => {
+    console.log(persons);
+}).catch((err) => {
+    console.error(err);
+});
--- HelloWorld/HelloWorld.ts
+++ HelloWorld/HelloWorld.ts
@@ -1,2 +1,26 @@
-let message: string = 'Hello World';
-console.log(message);
+
+var url = 'https://jsonplaceholder.typicode.com/users';
+
+type Persons = {
+    id: string;
+    name: string;
+}
+
+async function getPersons(path: string): Promise<Persons | null> {
+    const persons = await getData(path);
+    if (persons) {
+      return persons;
+    }
+    return null;
+}
+
+async function getData(path: string): Promise<Persons> {
+    const response = await fetch(path)
+    return await response.json();
+}
+
+getPersons(url).then((persons) => {
+    console.log(persons);
+}).catch((err) => {
+    console.error(err);
+});
